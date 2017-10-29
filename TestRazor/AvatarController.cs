49e920e157a4335f140c94aa79853dd56ef76100
Arnaud <arnaud.carpentier@outlook.com>
2017-10-29 18:04:23 +0100

    Initial commit
    Basic working application

---
+++ TestRazor/AvatarController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestRazor
+{
+    [Route("api/[controller]")]
+    public class AvatarController : ControllerBase
+    {
+        [HttpGet, Route("{id}")]
+        public IActionResult GetAvatar([FromRoute] string id)
+        {
+            if (id == "michelle")
+            {
+                return File(System.IO.File.OpenRead("images/michelle.png"), "images/jpeg");
+            }
+            if (id == "trump")
+            {
+                return File(System.IO.File.OpenRead("images/trump.jpg"), "images/jpeg");
+            }
+            return File(System.IO.File.OpenRead("images/unknown.jpg"), "images/jpeg");
+        }
+    }
+}
